4b97edc09359c58343cb778d9371fa611167ddfd
budi-injo <budi.injo@telepathy.ai>
2020-04-01 23:22:09 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        product: 'Socks',
+        message: 'helloworld',
+        image: 'socks-white.jpg',
+        url: 'http://www.google.com',
+        inventory: 0
+    }
+})
